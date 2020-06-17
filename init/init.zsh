2d48fd61668c3d92dda578c88d03ca2f45bedfcb
KiT O <keykakito@gmail.com>
2020-06-17 05:02:34 +0430

    Cleaned up git config a bit, a better startup script (kinda)
    Also added changing git master to main to the init script

---
+++ init/init.zsh
@@ -0,0 +1,26 @@
+#! /usr/bin/env zsh
+
+DIR=$(dirname ${BASH_SOURCE:-$0})
+PARENT_DIR="$PWD/$(dirname $DIR)"
+
+ln -s $PARENT_DIR/conf/zshrc $HOME/.zshrc
+ln -s $PARENT_DIR/conf/gemrc $HOME/.gemrc
+ln -s $PARENT_DIR/conf/taskrc $HOME/.taskrc
+ln -s $PARENT_DIR/conf/nanorc $HOME/.nanorc
+ln -s $PARENT_DIR/conf/tmux.conf $HOME/.tmux.conf.local
+ln -s $PARENT_DIR/packages/tmux/.tmux.conf $HOME/.tmux.conf
+
+GITCONFIG_FILE=$HOME/.gitconfig
+
+echo """[include]
+  path = $PARENT_DIR/git/gitconfig
+
+$(cat $GITCONFIG_FILE)
+""" > $GITCONFIG_FILE
+
+# Rename master to main in git
+cp -r /usr/share/git-core/templates ~/Templates/git.git
+echo 'ref: refs/heads/main' > ~/Templates/git.git/HEAD
+git config --global init.templateDir '~/Templates/git.git'
+
+sudo ln -s `pwd`/.kito/conf/apt.conf /etc/apt/apt.conf.d/00-my.conf
